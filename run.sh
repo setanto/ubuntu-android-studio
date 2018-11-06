17681a84e043a5c7907417d187de37234e0305cf
f4br1c3 <fabrice.aubaniac@gmail.com>
2018-11-06 12:17:04 +0100

    adding Dockerfile

--- run.sh
+++ run.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 if [ ! "$(docker ps -q -f name= ubuntu-android-studio)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name= ubuntu-android-studio)" ]; then
-        sudo docker start  ubuntu-android-studio
+  if [ "$(docker ps -aq -f status=exited -f name= ubuntu-android-studio)" ]; then
+    sudo docker start  ubuntu-android-studio
   else
-	sudo docker run -dit --net=host --env="DISPLAY" \
-        --name ubuntu-android-studio \
-        -v `pwd`/data/android-studio:/root -v /tmp/.X11-unix:/tmp/.X11-unix -v="$HOME/.Xauthority:/root/.Xauthority:rw" \
-        setanto/ubuntu-android-studio /opt/android-studio/bin/studio.sh 
-   fi
+    docker run -dit \
+    --privileged \
+    --net=host \
+    --env="DISPLAY" \
+    --name ubuntu-android-studio \
+    -v `pwd`/data/android-studio:/root \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v="$HOME/.Xauthority:/root/.Xauthority:rw" \
+    -v /dev/bus/usb:/dev/bus/usb \
+    -v /dev/kvm:/dev/kvm \
+    setanto/ubuntu-android-studio /opt/android-studio/bin/studio.sh
+  fi
 fi
